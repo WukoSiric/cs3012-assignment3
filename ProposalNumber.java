2c507b00c3a38cd76c7475873d10eebd424122a8
Luke Siric Stainer <lstainer2002@gmail.com>
2023-11-09 23:47:36 +1030

    Handle Proposal numbers

---
+++ ProposalNumber.java
@@ -0,0 +1,35 @@
+public class ProposalNumber {
+    private String proposalNumber;
+    private String nodeName; //M1
+    private int roundNumber;
+
+    public ProposalNumber(String nodeName, int roundNumber) {
+        this.nodeName = nodeName;
+        this.roundNumber = roundNumber;
+
+        this.proposalNumber = roundNumber + "." + nodeName.substring(1);
+    }
+
+    public void incrementRoundNumber() {
+        this.roundNumber++;
+        this.proposalNumber = roundNumber + "." + nodeName.substring(1);
+    }
+
+    public String getProposalNumber() {
+        return proposalNumber;
+    }
+
+    public Boolean isGreaterThanCurrent(String other) {
+        String[] otherSplit = other.split("\\.");
+        int otherRoundNumber = Integer.parseInt(otherSplit[0]);
+        String otherNodeName = otherSplit[1];
+
+        if (otherRoundNumber > this.roundNumber) {
+            return false;
+        } else if (otherRoundNumber < this.roundNumber) {
+            return true;
+        } else {
+            return otherNodeName.compareTo(this.nodeName.substring(1)) > 0;
+        }
+    }
+}
