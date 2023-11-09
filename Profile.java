b706fa494df616a4267c267343e9f720c0e36baa
Luke Siric Stainer <lstainer2002@gmail.com>
2023-11-10 09:23:48 +1030

    Refactor Profile class to use uppercase profile names and add message delay and drop probability methods.

--- Profile.java
+++ Profile.java
@@ -5,21 +5,23 @@ public class Profile {
     }
 
     CurrentProfile profile; 
+
+
     Profile(String profile) {
-        switch (profile.toLowerCase()) {
-            case "instant":
+        switch (profile.toUpperCase()) {
+            case "INSTANT":
                 this.profile = CurrentProfile.INSTANT;
                 break;
-            case "m2":
+            case "M2":
                 this.profile = CurrentProfile.M2;
                 break;
-            case "m3":
+            case "M3":
                 this.profile = CurrentProfile.M3;
                 break;
-            case "small":
+            case "SMALL":
                 this.profile = CurrentProfile.SMALL;
                 break;
-            case "large":
+            case "LARGE":
                 this.profile = CurrentProfile.LARGE;
                 break;
             default:
@@ -27,5 +29,40 @@ public class Profile {
                 break;
         }
     }
+
+    public int getMessageDelayMilliseconds() {
+        switch (this.profile) {
+            case INSTANT:
+                return 0;
+            case M2:
+                return 4000;
+            case M3:
+                return 1000;
+            case SMALL:
+                return 500;
+            case LARGE:
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public double getMessageDropProbability() {
+        switch (this.profile) {
+            case INSTANT:
+                return 0.0;
+            case M2:
+                return 0.2;
+            case M3:
+                return 0.15;
+            case SMALL:
+                return 0.05;
+            case LARGE:
+                return 0.1;
+            default:
+                return 0.0;
+        }
+    }
+
 
 }
