66045d1e0ee54baa505b9df33d42be5c1e4c4b6c
Luke Siric Stainer <lstainer2002@gmail.com>
2023-11-09 23:47:22 +1030

    Started message construction

--- MessageConstructor.java
+++ MessageConstructor.java
@@ -1,7 +1,31 @@
 public class MessageConstructor {
     MessageType type; // Enum: PREPARE, PROMISE, ACCEPT
-    
+    // MESSAGE STRUCTURE TO:FROM:TYPE:PROPOSALNUMBER:VALUE 
+
     enum MessageType {
         PREPARE, PROMISE, ACCEPT, ACCEPT_REQUEST 
     }
+
+    public String makePrepare(String to, String from, String proposalNumber) {
+        this.type = MessageType.PREPARE;
+        return to + ":" + from + ":" + type + ":" + proposalNumber;
+    }
+
+    public String makePromise(String to, String from, String proposalNumber, String value) {
+        this.type = MessageType.PROMISE;
+        return to + ":" + from + ":" + type + ":" + proposalNumber + ":" + value;
+    }
+
+    public String makePromise(String to, String from, String proposalNumber) {
+        this.type = MessageType.PROMISE;
+        return to + ":" + from + ":" + type + ":" + proposalNumber;
+    }
+
+    public String makePropose(String to, String from, String proposalNumber, String value) {
+        this.type = MessageType.ACCEPT;
+        return to + ":" + from + ":" + type + ":" + proposalNumber + ":" + value;
+    }
+
+    
+
 }
